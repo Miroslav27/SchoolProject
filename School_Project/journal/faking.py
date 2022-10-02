09dd961a0e8f57819dc981abbd46607fdcccf2e3
Мирослав Тюрін <miroslav.tyurin@gmail.com>
2022-10-03 00:56:04 +0300

    Printed 8 queries

--- School_Project/journal/faking.py
+++ School_Project/journal/faking.py
@@ -11,18 +11,21 @@ def add_fake_student(quantity = 1):
         fake_group = random.choice(Group.objects.all())
         fake_student=Student(name=fake_name, email=fake_email, age=fake_age, group=fake_group, dummy=True)
         fake_student.save()
-        print(f"fake {i} is ready!")
+        print(f"fake {i+1} is ready!")
 
 def add_fake_teacher(quantity = 1):
     fake=faker.Faker()
+    rank=["Doc.","Proff","Sir",]
     for i in range(quantity):
-        fake_name = fake.name()
-        fake_email = f"{fake_name.lower().replace(' ','.')}@{fake.domain_name()}"
+        fake_name = f"{random.choice(rank)} {fake.name()}"
+        fake_email = f" {fake_name.lower().replace(' ','.')}@{fake.domain_name()}"
         fake_age = random.randint(random.randint(16, 99), 99)
         fake_group = random.choice(Group.objects.all())
-        fake_student=Teacher(name=fake_name, email=fake_email, age=fake_age, group=fake_group,dummy=True)
-        fake_student.save()
-        print(f"fake {i} is ready!")
+        fake_teacher=Teacher(name=fake_name, email=fake_email, age=fake_age, dummy=True)
+        fake_teacher.save()
+        fake_teacher.groups.set([fake_group.id])
+
+        print(f"fake {i+1} is ready!")
 
 def delete_all_fakes():
     Teacher.objects.filter(dummy=True).delete()
--- School_Project/journal/models.py
+++ School_Project/journal/models.py
@@ -19,9 +19,9 @@ email"""
 class Group(models.Model):
 
     name = models.CharField(max_length=255)
-    Teacher = models.ManyToManyField("journal.Teacher")
+    #teacher = models.ManyToManyField("journal.Teacher")
     def __str__(self):
-        return self.name
+        return f"{self.name} "
 
 
 class Teacher(models.Model):
@@ -29,9 +29,9 @@ class Teacher(models.Model):
     age = models.PositiveSmallIntegerField()
     email = models.CharField(max_length=255)
     dummy = models.BooleanField(default=False)
-
+    groups = models.ManyToManyField("journal.Group", blank=True )
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.age} "
 
 class Student(models.Model):
     name = models.CharField(max_length=255)
@@ -40,4 +40,4 @@ class Student(models.Model):
     group = models.ForeignKey("journal.Group", on_delete=models.SET_NULL, null=True)
     dummy = models.BooleanField(default=False)
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.age} {self.group}"
